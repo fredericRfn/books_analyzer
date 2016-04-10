25a9b976b274222e01c9c8af2607cb38f7b4665e
fredericRfn <723278@unizar.es>
2016-04-10 11:42:03 +0200

    Separate the generation of sentences from the generation of characters in order to anticipate the ability to retrieve the sentences from the database

--- src/main/java/com/books_analyzer/Book.java
+++ src/main/java/com/books_analyzer/Book.java
@@ -11,47 +11,56 @@ public class Book {
 	public final String title;
 	public ArrayList<Character> characters;
 	public ArrayList<Sentence> sentences;
+	//public ArrayList<CharacterSentence> characterSentence;
 	
-	public Book(String t, String c) {
+	public Book(String t, String content) {
 		this.title = t;
-		generateSentences(c);
+		this.characters = new ArrayList<Character>();
+		this.sentences = new ArrayList<Sentence>();
+		//this.characterSentence = new ArrayList<CharacterSentence>();
+		generateSentences(content);
+		generateCharacters();
+		//generateAssociations();
 	}
 	
 
 	private void generateSentences(String cont) {
 		String c = cont.replace("\r", "");
 		String[] rawSentences = c.split("\n|\\.(?!\\d)|(?<!\\d)\\.");
-		ArrayList<Sentence> s = new ArrayList<Sentence>();
-		ArrayList<Character> ch = new ArrayList<Character>();
-		HashMap<String, Integer> wordsAndCount = new HashMap<String, Integer>();
-		HashMap<String, Integer> wordsUpperAndCount = new HashMap<String, Integer>();
-		int count;
 		for (int i=0; i<rawSentences.length; i++) {
-			String[] words = rawSentences[i].replace("\\", "").replace("'s", "").replace("\"", "").replace(".", "").replace(",", "").replace("?", "").replace("!","").split("\\s|'");
+			sentences.add(new Sentence(rawSentences[i]));
+		}
+	}
+	
+	public void generateCharacters() {
+		String[] words;
+		int count;
+		HashMap<String, Integer> wordsWithCount = new HashMap<String, Integer>();
+		HashMap<String, Integer> wordsUpperWithCount = new HashMap<String, Integer>();
+		for(Sentence tmp: this.sentences) {
+			words = tmp.getWords();
 			if (words.length > 1) {
 				for (int j=0; j<words.length; j++) {
 					if (!words[j].isEmpty()) {
 						if (java.lang.Character.isUpperCase(words[j].charAt(0))) {
-							count = wordsUpperAndCount.containsKey(words[j].toLowerCase()) ? wordsUpperAndCount.get(words[j].toLowerCase()) : 0;
-							wordsUpperAndCount.put(words[j].toLowerCase(), count + 1); 
+							count = wordsUpperWithCount.containsKey(words[j].toLowerCase()) ? wordsUpperWithCount.get(words[j].toLowerCase()) : 0;
+							wordsUpperWithCount.put(words[j].toLowerCase(), count + 1); 
 						}
 					}
-					count = wordsAndCount.containsKey(words[j].toLowerCase()) ? wordsAndCount.get(words[j].toLowerCase()) : 0;
-					wordsAndCount.put(words[j].toLowerCase(), count + 1); 
+					count = wordsWithCount.containsKey(words[j].toLowerCase()) ? wordsWithCount.get(words[j].toLowerCase()) : 0;
+					wordsWithCount.put(words[j].toLowerCase(), count + 1); 
 				}
 			}
-			s.add(new Sentence(rawSentences[i]));
 		}
-		Set<String> keySet = wordsUpperAndCount.keySet();
+		Set<String> keySet = wordsUpperWithCount.keySet();
 		Iterator iterator = keySet.iterator();
 	    String tmp;
 		while(iterator.hasNext()) {
 	    	tmp = (String) iterator.next();
-	    	if ((wordsUpperAndCount.get(tmp) == wordsAndCount.get(tmp)) &&  (wordsUpperAndCount.get(tmp) > 2)) {
-	    		ch.add(new Character(tmp));
+	    	if ((wordsUpperWithCount.get(tmp) == wordsWithCount.get(tmp)) &&  (wordsUpperWithCount.get(tmp) > 2)) {
+	    		characters.add(new Character(tmp));
 	    	}
 	    }
-		this.sentences = s;
-		this.characters = ch;
 	}
+	
 }
--- src/main/java/com/books_analyzer/Sentence.java
+++ src/main/java/com/books_analyzer/Sentence.java
@@ -2,9 +2,19 @@ package com.books_analyzer;
 
 public class Sentence {
 	public final String content;
+	private String[] words;
 	
 	public Sentence(String s) {
 		this.content = s;
+		this.words = s.replace("\\", "").replace("'s", "").replace("\"", "").replace(".", "").replace(",", "").replace("?", "").replace("!","").split("\\s|'");
+	}
+	
+	public String getContent() {
+		return this.content;
+	}
+	
+	public String[] getWords() {
+		return this.words;
 	}
 
 }
