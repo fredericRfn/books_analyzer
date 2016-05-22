d5f6b3c5b9934decc5011b9e37e2216fb9c905c0
fredericRfn <723278@unizar.es>
2016-05-22 19:19:26 +0200

    Second attempt to complete the CORS integration

--- src/main/java/com/books_analyzer/Application.java
+++ src/main/java/com/books_analyzer/Application.java
@@ -4,11 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/search").allowedOrigins("localhost:8080");
+                registry.addMapping("/search").allowedOrigins("https://client-books-analyzer.herokuapp.com");
+                registry.addMapping("/search").allowedOrigins("https://api-books-analyzer.herokuapp.com");
+            }
+        };
     }
 }
