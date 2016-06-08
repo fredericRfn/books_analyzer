246d56f3d98d0ae789e9bdedb9302ce87f8de21c
fredericRfn <723278@unizar.es>
2016-06-08 09:15:27 +0200

    Rename controllers in order to respect the last defined software design

--- src/main/java/com/books_analyzer_ws/controller/ClientController.java
+++ src/main/java/com/books_analyzer_ws/controller/ClientController.java
@@ -1,18 +1,25 @@
 package com.books_analyzer_ws.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.twitter.api.SearchResults;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.books_analyzer_ws.service.BooksService;
 
-@Controller
+@RestController
 public class ClientController {
-
-    @RequestMapping("/")
-    public String greeting() {
-        return "index";
+	@CrossOrigin
+    @RequestMapping(value = "/search", method = { RequestMethod.GET })
+    public String search(
+		@RequestParam(value = "title", required=false) String title,
+		@RequestParam(value = "author", required=false) String author,
+		@RequestParam(value = "character", required=false) String character,
+		@RequestParam(value = "url", required=false) String url,
+		@RequestParam(value = "callback", required=false) String callback
+    )  {
+    	BooksService booksService = new BooksService();
+    	return booksService.buildResponse(title, author, character, url);
     }
 }
