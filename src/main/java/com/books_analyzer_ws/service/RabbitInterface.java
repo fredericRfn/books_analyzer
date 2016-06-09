6271a3dd0d7520f058918f0d5cbc49565af2cdcc
fredericRfn <723278@unizar.es>
2016-06-09 03:55:00 +0200

    Make the RabbitInterface able to connect to the newly configured CloudAMQP

--- src/main/java/com/books_analyzer_ws/service/RabbitInterface.java
+++ src/main/java/com/books_analyzer_ws/service/RabbitInterface.java
@@ -12,14 +12,18 @@ public class RabbitInterface {
 	private final static String QUEUE_NAME = "jobs_queue";
 	
 	public static boolean requestAnalysis(String id) {
+		System.out.println("Inside RabbitMQ interface with id:" + id);
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("54.191.210.230");
+		factory.setHost("fox.rmq.cloudamqp.com");
+		factory.setUsername("xhffrluv");
+		factory.setPassword("zIU2WWJtvjsDUv_BrwCoU60RWcekbpvP");
+		factory.setVirtualHost("xhffrluv");
 		Connection connection;
 		try {
 			 connection = factory.newConnection();
 			 Channel channel = connection.createChannel();	 
 			 channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-			 String message = Integer.valueOf(id).toString();
+			 String message = id;
 			 channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
 			 System.out.println(" [x] Sent '" + message + "'");
 			 channel.close();
